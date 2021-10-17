e57f906bc24bfc69de3a459fe2d9c40dc5dc284f
iansaf <iansaf@ga.github.io>
2021-10-17 17:24:08 +0800

    q

---
+++ V0.2/mask.js
@@ -0,0 +1,29 @@
+var link = document.createElement('link');
+
+link.type = 'text/css';
+link.rel = 'stylesheet';
+link.href = "mask.css";
+
+var head = document.getElementsByTagName('head')[0];
+head.appendChild(link);
+
+function clickmask (e) {
+    console.log(e.srcElement.classList.remove("mask"));
+    //t.classList.add("maskunload");
+}
+
+var mask = {
+    add: (selector) => {
+        setTimeout(() => {
+            document.querySelectorAll(selector).forEach((tm) => {
+                tm.classList.add("mask");
+
+                tm.addEventListener('click', clickmask);
+            })
+            // var tm = document.querySelectorAll(selector);
+            // tm.classList.add("mask");
+
+            // tm.addEventListener('click', clickmask);    
+        }, 100)
+    }
+}
